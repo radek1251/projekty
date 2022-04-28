9e7ca67237ca58f26b2312b40dfbae4c54f67fb7
radek1251 <102186047+radek1251@users.noreply.github.com>
2022-04-28 16:38:33 +0200

    Add files via upload

---
+++ DOM-form/js/script.js
@@ -0,0 +1,19 @@
+
+
+const submitForm = (event) => {
+    
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name = "fname"]');
+    let lastName = document.querySelector('[name = "lname"]');
+    
+    console.log(`Imię: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+let form = document.getElementById('form'); 
+form.addEventListener('submit', submitForm);
+
+console.log(form);
+
+
+
